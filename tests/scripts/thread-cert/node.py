ad4959b4e53f7a135cecb72d7d4b1d5c63a0f9c7
Nick Banks <nibanks@microsoft.com>
2016-10-24 12:14:54 -0700

    Fix merge issue.

--- tests/scripts/thread-cert/node.py
+++ tests/scripts/thread-cert/node.py
@@ -507,11 +507,6 @@ class Node:
             self.send_command(cmd)
             self.pexpect.expect('Done')
 
-    def set_max_children(self, number):
-        cmd = 'childmax %d' % number
-        self.send_command(cmd)
-        self.pexpect.expect('Done')
-
     def get_weight(self):
         if self.Api:
             return int(self.Api.otNodeGetWeight(self.otNode))
